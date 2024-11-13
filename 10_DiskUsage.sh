d983c4db7ae19adf4ff635c58320a54a147d5e73
kalyaneswar <kalyaneswarreddy16696@gmail.com>
2024-11-13 08:42:46 +0530

    scripting

---
+++ 10_DiskUsage.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# **Check Disk Usage**
+#     - Write a script that checks the disk usage of the system and reports if the disk usage is over a certain threshold.
+
+df -h
+
+disk_usage=(df / | grep / | awk '{ print $6 }')
+
+if [ $disk_usage > 80% ] then
+    echo "disk usage is Over a certain threshold"
+else
+    echo "disk usage is Under a certain threshold"
+fi
